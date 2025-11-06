f980e13f7d7e2539543bb2856b3537814a45f9b6
Angela Cordero <angela.cordero@correounivalle.edu.co>
2025-11-05 19:44:59 -0500

    Added images

--- js/main.js
+++ js/main.js
@@ -1,58 +1,63 @@
 const products = [
   {
     id: 1,
-    name: "pizza",
+    name: "Lasagna",
     price: 10,
+    URL: "https://i.postimg.cc/8jVYY5L2/lasagna.jpg",
   },
   {
     id: 2,
-    name: "hotdog",
+    name: "Pizza",
     price: 8,
+    URL: "https://i.postimg.cc/w7drrvhd/pizza.jpg",
   },
   {
     id: 3,
-    name: "burger",
+    name: "Burger",
     price: 15,
+    URL: "https://i.postimg.cc/9z9S1Tws/burger.jpg",
   },
   {
     id: 4,
-    name: "sandwich",
+    name: "Sandwich",
     price: 13,
+    URL: "https://i.postimg.cc/kRWp43d2/sandwich.jpg[/img",
   },
   {
     id: 5,
-    name: "soda",
+    name: "Iced tea",
     price: 3,
+    URL: "https://i.postimg.cc/Sj95V8zk/iced-tea.jpg",
   },
   {
     id: 6,
-    name: "lemonade",
+    name: "Lemonade",
     price: 4,
+    URL: "https://i.postimg.cc/4mT00dpr/lemonade.jpg",
   },
   {
     id: 7,
-    name: "beer",
+    name: "Banana smothie",
     price: 5,
+    URL: "https://i.postimg.cc/F1SqGckq/banana-smothie.jpg[/img",
   },
   {
     id: 8,
     name: "cake",
     price: 11,
+    URL: "https://i.postimg.cc/23WJTvBp/cake.jpg",
   },
   {
     id: 9,
-    name: "ice-cream",
+    name: "Chocolate mousse",
     price: 7,
+    URL: "https://i.postimg.cc/nMm5T7QJ/chocolate-mousse.jpg",
   },
   {
     id: 10,
-    name: "brownie",
+    name: "Lemon cheesecake",
     price: 5,
-  },
-  {
-    id: 11,
-    name: "cookies",
-    price: 2,
+    URL: "https://i.postimg.cc/4ncMwtHq/cheesecake.jpg",
   },
 ];
 
@@ -62,8 +67,10 @@ function renderProducts(productsArray) {
   productsArray.forEach((product) => {
     const card = document.createElement("div");
     card.classList.add("products");
-    card.innerHTML = `<h4>${product.name}</h4>
-    <p>${product.price}</p>
+    card.innerHTML = `<img src="${product.URL}" 
+      alt="${product.name}" width="120">
+    <h4>${product.name}</h4>
+    <p>$${product.price}</p>
     <button class="buttonAddToOrder" id= "${product.id}">Add to order</button>`;
     productsContainer.appendChild(card);
   });
@@ -95,13 +102,15 @@ renderOrderContainer(orderedProducts);
 function renderOrderContainer(orderedProducts) {
   let orderContainer = document.getElementById("orderContainer");
 
-  orderContainer.innerHTML = "";
+  orderContainer.innerHTML = "Your order";
 
   if (orderedProducts.length > 0) {
     orderedProducts.forEach((product) => {
       const card = document.createElement("div");
-      card.innerHTML = `<h4>${product.name}</h4>
-                    <p>${product.price}</p>
+      card.innerHTML = `<img src="${product.URL}" 
+      alt="${product.name}" width="50">
+                    <h4>${product.name}</h4>
+                    <p>$${product.price}</p>
                     <button class="buttonRemoveFromOrder" id= "${product.id}">Remove</button>`;
       orderContainer.appendChild(card);
     });
